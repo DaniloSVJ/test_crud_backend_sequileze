2c14c47ea623c7298d1a6ad9f639f8d7a695aa33
DaniloSVJ <danilocotatoceu@gmail.com>
2022-03-21 20:25:20 -0300

    add

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: "postgres",
-    host:"localhost",
-    username: "postgres",
+    host:"ec2-44-199-40-188.compute-1.amazonaws.com",
+    username: "fzjrhhvprgcrbj",
     password: "1234567",
-    database: "test_crud",
+    database: "7495950c21c8c330cabccde7fb16396cee7fbd2d3da0b56f3a12415ae92b2f8f",
     define: {
         timestamp:true,
         underscored: true,
