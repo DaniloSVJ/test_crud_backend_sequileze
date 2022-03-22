c2f87ad780d6374594becd58e72a828aa6fe60e6
DaniloSVJ <danilocotatoceu@gmail.com>
2022-03-21 21:50:53 -0300

    add

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,20 @@
 module.exports = {
     dialect: "postgres",
+    protocol: 'postgres',
     host:"ec2-44-199-40-188.compute-1.amazonaws.com",
     username: "fzjrhhvprgcrbj",
-    password: "1234567",
-    database: "7495950c21c8c330cabccde7fb16396cee7fbd2d3da0b56f3a12415ae92b2f8f",
+    password: "7495950c21c8c330cabccde7fb16396cee7fbd2d3da0b56f3a12415ae92b2f8f",
+    port:5432,
+    database: "dehifn2t94l4h0",
+    ssl: true, 
     define: {
         timestamp:true,
         underscored: true,
     },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+          }
+      }
 }
